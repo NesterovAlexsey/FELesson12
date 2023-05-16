1b5eddbf89a0989f66c8b933abb26bbbbdc6e920
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-16 06:49:10 +0200

    finish with task 1

--- FEhomework12.js
+++ FEhomework12.js
@@ -1,14 +1,18 @@
 // Задание 1
-// У вас есть массив объектов:
-
-// const cars = [
-//     { brand: "BMW", price: 20000, isDiesel: true },
-//     { brand: "Mercedes", price: 22000, isDiesel: false },
-//     { brand: "Porshe", price: 50000, isDiesel: true },
-//     { brand: "Nissan", price: 25000, isDiesel: false },
-// ];
+// У вас есть массив объектов:car
 // Создайте на основе страрого массива новый массив объектов по образу: 
 // [{brand: "BMW", isDiesel: true}, {brand: "Mercedes", isDiesel: false}...]
+
+const cars = [
+    { brand: "BMW", price: 20000, isDiesel: true },
+    { brand: "Mercedes", price: 22000, isDiesel: false },
+    { brand: "Porshe", price: 50000, isDiesel: true },
+    { brand: "Nissan", price: 25000, isDiesel: false },
+];
+
+const carNameAndOil = cars.map((car) => {
+    return `brand: "${car.brand}", isDisel: ${car.isDiesel}`
+}) 
 
 // Задание 2
 // Создайте новый массив, где оставьте только машины с дизельным двигателем.
@@ -25,3 +29,5 @@
 // Задание 6
 // Создайте новый массив, где все дизельные машины заменены на 
 // { brand: "Tesla", price: 25000, isDiesel: false }
+
+
